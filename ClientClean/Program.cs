ff6fd4a5986a7ff91a8ca88422f7f0d9eaed0c4a
Marco Diniz <msn@marcodiniz.com>
2023-02-15 01:03:00 +0000

    cleaned version

--- ClientClean/Program.cs
+++ ClientClean/Program.cs
@@ -1,18 +1,16 @@
-using AccuChat.Client;
+using AccuChat.ClientClean;
 using AccuChat.Shared;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 
-
 builder.Services.AddLogging(b =>
 {
-    b.AddFilter("Microsoft", LogLevel.Warning)
-     .AddFilter("System", LogLevel.Warning)
-     .AddFilter("", LogLevel.Information);  
+    b.AddFilter("Microsoft", LogLevel.Warning);
+    b.AddFilter("System", LogLevel.Warning);
+    b.AddFilter("", LogLevel.Information);  
 });
 
-builder.Services.AddSingleton<GameServer>();
 
 await builder.Build().RunAsync();
--- Shared/GameServer.cs
+++ Shared/GameServer.cs
@@ -1,42 +1,44 @@
 ﻿using Microsoft.AspNetCore.SignalR.Client;
-namespace AccuChat.Client;
+using Microsoft.Extensions.Logging;
+
+namespace AccuChat.Shared;
 
 public class GameServer
 {
-	public HubConnection _conn;
+    public HubConnection _conn;
     private Player _me;
     private readonly ILogger<GameServer> _log;
     private Dictionary<string, Player> _players = new();
     public ICollection<Player> Players => _players.Values;
     public event EventHandler<PlayersChangedEventArgs> PlayersChanged;
 
-	public GameServer(ILogger<GameServer> log)
-	{
-		_log = log;
-		_log.LogDebug("HubClient constructed");
+    public GameServer(ILogger<GameServer> log)
+    {
+        _log = log;
+        _log.LogDebug("HubClient constructed");
         _conn = new HubConnectionBuilder()
             .WithAutomaticReconnect()
-			.WithUrl("https://accuchat.loca.lt/hub", o => o.Headers.Add("Bypass-Tunnel-Reminder", "anything"))
-			//.WithUrl("https://qrpg2mb5-7243.uks1.devtunnels.ms/hub")
-			.Build();
-	}
+            .WithUrl("https://accuchat.loca.lt/hub", o => o.Headers.Add("Bypass-Tunnel-Reminder", "anything"))
+            //.WithUrl("https://qrpg2mb5-7243.uks1.devtunnels.ms/hub")
+            .Build();
+    }
 
-	public async Task StartAsync()
-	{
-		_conn.Closed += async e => _log.LogWarning("Connection closed");
-		_conn.Reconnecting += async e => _log.LogInformation("Connection closed");
-		_conn.Reconnected += async e => _log.LogInformation("Reconnected");
+    public async Task StartAsync()
+    {
+        _conn.Closed += async e => _log.LogWarning("Connection closed");
+        _conn.Reconnecting += async e => _log.LogInformation("Connection closed");
+        _conn.Reconnected += async e => _log.LogInformation("Reconnected");
 
-		_conn.On(nameof(IHubClient.OnPong), OnPong);
+        _conn.On(nameof(IHubClient.OnPong), OnPong);
         _conn.On<PlayerActionEventArgs>(nameof(IHubClient.OnPlayerAction), OnPlayerUpdated);
         _conn.On<PlayersChangedEventArgs>(nameof(IHubClient.OnPlayersChanged), OnPlayersChanged);
         //_conn.On<PlayerCard>(nameof(OnPlayerEnteredRoom), OnPlayerEnteredRoom);
 
         await _conn.StartAsync();
 
-	}
+    }
 
-    public Task<string> Ping() =>  _conn.InvokeAsync<string>(nameof(IHubHost.Ping));
+    public Task<string> Ping() => _conn.InvokeAsync<string>(nameof(IHubHost.Ping));
     public async Task Join(string name)
     {
         _me = await _conn.InvokeAsync<Player>(nameof(IHubHost.Join), name);
@@ -46,7 +48,7 @@ public class GameServer
     {
         await _conn.SendAsync(nameof(IHubHost.Move), direction);
     }
-    
+
     public async Task Speak(string message)
     {
         await _conn.SendAsync(nameof(IHubHost.Speak), message);
@@ -60,7 +62,7 @@ public class GameServer
     private void OnPlayersChanged(PlayersChangedEventArgs args)
     {
         _log.LogInformation("Received players change {args}", args);
-        
+
         var p = _GetOrAddPlayer(args.Player);
         switch (args.ConnectionChange)
         {
@@ -94,7 +96,7 @@ public class GameServer
             default:
                 break;
         }
- 
+
         p.AvatarCode = args.Player.AvatarCode;
         p.Name = args.Player.Name;
 
