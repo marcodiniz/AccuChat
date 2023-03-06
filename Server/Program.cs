d52228e6afcdc1ddf5e783a089e9af1d767a3f60
Marco Diniz <marco@accurx.com>
2023-03-06 13:39:30 +0000

    small fixes

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,12 +1,14 @@
 using AccuChat.Server;
 using Microsoft.AspNetCore.ResponseCompression;
+using System.Globalization;
 
 var builder = WebApplication.CreateBuilder(args);
-
 
 builder.Services.AddSingleton<GameStore>();
 builder.Services.AddSignalR();
 builder.Services.AddCors();
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
 builder.Services.AddLogging(b =>
 {
     b.AddFilter("Microsoft", LogLevel.Warning)
@@ -15,13 +17,33 @@ builder.Services.AddLogging(b =>
 
 var app = builder.Build();
 
-app.MapHub<ServerHub>("/hub");
-app.UseCors(c =>
-{
-    c.AllowAnyOrigin().AllowAnyHeader();
-});
-app.Logger.LogInformation("starting");
+app.UseHttpsRedirection();
+app.UseBlazorFrameworkFiles();
 
+var staticOptions = new StaticFileOptions();
+staticOptions.OnPrepareResponse = (r) =>
+{
+    r.Context.Response.Headers["Bypass-Tunnel-Reminder"] = "yes";
+};
+app.UseStaticFiles(staticOptions);
+
+app.UseRouting();
+app.MapRazorPages();
+app.MapControllers();
+app.MapFallbackToFile("index.html");
+
+app.MapHub<ServerHub>("/hub");
+//app.UseCors(c =>
+//{
+//    c.AllowAnyOrigin().AllowAnyHeader();
+//});
+app.Logger.LogInformation("starting");
+app.Use(async (context, next) =>
+{
+    await next(context);
+    context.Response.Headers["Bypass-Tunnel-Reminder"] = "yes";
+    context.Request.Headers["Bypass-Tunnel-Reminder"] = "yes";
+});
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -35,6 +57,5 @@ else
 }
 
 app.UseHttpsRedirection();
-
 
 app.Run();
